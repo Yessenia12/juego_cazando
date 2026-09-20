dec74f066f70b56a80a3898535f7ebb653423dec
Yessenia12 <quinatoalemayessenia@gmail.com>
2026-09-19 23:57:48 -0500

    Creacion de la funcion graficarComida y definicion de variables y constantes

--- cazando.js
+++ cazando.js
@@ -1,7 +1,28 @@
 let canvas=document.getElementById("areaJuego");
 let ctx=canvas.getContext("2d");
+let gatoX =0;
+let gatoY=0;
+let comidaX=0;
+let comidaY=0;
+const ALTO_GATO=50;
+const ANCHO_GATO=35;
+const ALTO_COMIDA=25;
+const ANCHO_COMIDA=25;
+
 
 function graficarGato(){
     ctx.fillStyle="red";
-    ctx.fillRect(250,225,25,50);
+    ctx.fillRect(gatoX,gatoY,ANCHO_GATO,ALTO_GATO);
+}
+function graficarComida(){
+    ctx.fillStyle="green";
+    ctx.fillRect(comidaX,comidaY,ANCHO_COMIDA,ALTO_COMIDA);
+}
+function iniciarJuego(){
+    gatoX = (500 - ANCHO_GATO) / 2;
+    gatoY = (500 - ALTO_GATO) / 2;
+    comidaX = 500 - ANCHO_COMIDA;
+    comidaY = 500 - ALTO_COMIDA;
+    graficarGato();
+    graficarComida();
 }
